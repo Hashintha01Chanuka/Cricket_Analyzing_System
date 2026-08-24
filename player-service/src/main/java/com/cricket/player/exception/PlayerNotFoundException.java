ff283de7c5ca87edf9b84620fd9517900a15fb6d
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2026-08-24 09:28:50 +0530

    create general and player not found exception

---
+++ player-service/src/main/java/com/cricket/player/exception/PlayerNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cricket.player.exception;
+
+import java.util.UUID;
+
+public class PlayerNotFoundException extends RuntimeException {
+
+    public PlayerNotFoundException(UUID playerId) {
+        super("Player not found: " + playerId);
+    }
+}
